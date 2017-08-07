e3f4f43f5c22ae94bee1ac673e4f1b8d3f169b74
dengle3 <dengle3@wanda.cn>
2017-08-07 17:44:26 +0800

    add

---
+++ SwiftSample/SwiftSample/Classes/Module/Home/HuiHomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HuiHomeViewController.swift
+//  SwiftSample
+//
+//  Created by 邓乐 on 2017/8/7.
+//  Copyright © 2017年 Hui. All rights reserved.
+//
+
+import UIKit
+
+class HuiHomeViewController: HuiBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
